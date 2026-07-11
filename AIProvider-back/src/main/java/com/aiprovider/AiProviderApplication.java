022a7d8e7de0654761e4b99619599f0f35a6d86f
wendy <492137544@qq.com>
2026-07-11 19:40:11 +0800

    init: Spring Boot backend + React frontend

---
+++ AIProvider-back/src/main/java/com/aiprovider/AiProviderApplication.java
@@ -0,0 +1,12 @@
+package com.aiprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProviderApplication.class, args);
+    }
+}
