19660c9daf5a59a7c8d643e8d3cef6d124c8dff7
XiangJingYi <492137544@qq.com>
2026-07-12 06:21:22 +0800

    缩小首页图片 10%

--- AIProvider-front/src/App.jsx
+++ AIProvider-front/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={heroImg} className="base" width="153" height="161" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
